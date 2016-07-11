21fefa0755ce079c6840f66141f8142d3e642076
Sam Agnew <sagnew@eden.rutgers.edu>
2016-07-11 16:36:14 -0400

    Replace animate.js with game.js and update code to the talk I am giving

---
+++ static/game.js
@@ -0,0 +1,89 @@
+const BALL_SPEED = 3;
+
+const canvas = document.getElementById('screen');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let elements = [];
+
+class Element {
+  constructor(x, y, width, height, vx, vy) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.vx = vx || 0;
+    this.vy = vy || 0;
+
+    elements.push(this);
+  }
+
+  draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  move() {
+    elements.forEach((element) => {
+      // Bounce on horizontal collision
+      if ((this.top() < element.bottom() && this.bottom() > element.top()) &&
+          (this.right() < element.left() && this.right() + this.vx >= element.left() ||
+           this.left() > element.right() && this.left() + this.vx <= element.right())) {
+        this.vx = -this.vx;
+        return;
+      }
+
+      // Bounce on vertical collision
+      if ((this.left() < element.right() && this.right() > element.left()) &&
+          (this.bottom() < element.top() && this.bottom() + this.vy >= element.top() ||
+           this.top() > element.bottom() && this.top() + this.vy <= element.bottom())) {
+        this.vy = -this.vy;
+        return;
+      }
+    });
+
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  left() {
+    return this.x;
+  }
+
+  right() {
+    return this.x + this.width;
+  }
+
+  top() {
+    return this.y;
+  }
+
+  bottom() {
+    return this.y + this.height;
+  }
+}
+
+let paddle1 = new Element(5, canvas.height/2, 20, 200);
+let paddle2 = new Element(canvas.width - 55, canvas.height/2, 20, 200);
+let ball = new Element(canvas.width/2, canvas.height/2, 30, 30, BALL_SPEED, 0.6 * BALL_SPEED);
+
+let topWall = new Element(0, 0, canvas.width, 1);
+let bottom = new Element(0, canvas.height - 1, canvas.width, 1);
+
+let gameLoop = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  elements.forEach((element) => {
+    element.move();
+    element.draw();
+  });
+
+  if (ball.right() > canvas.width || ball.left() < 0) {
+    ball.x = canvas.width/2;
+    ball.y = canvas.height/2;
+  }
+
+  window.requestAnimationFrame(gameLoop);
+};
+
+window.requestAnimationFrame(gameLoop);
